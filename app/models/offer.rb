77db8928055883ea1bd82669a65868c8c9af110c
santino doucet <doucetsantino@gmail.com>
2024-08-12 16:33:14 +0200

    added validations and associations

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,3 +1,8 @@
 class Offer < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+  validates :title, presence: true
+  validates :description, presence: true, length: { in: 20..500 }
+  validates :localisation, presence: true
+  validates :prix, presence: true, comparison: { greater_than: 1 }
 end
